2333fe3eb0f1635c32b6db08e9603d43bd635a31
LWSE13 <leewheeler888@gmail.com>
2024-02-02 21:18:13 +0000

    added notes explaining base code to better help me understand what's going on. Added my first four string variables and 4 password preference confirm boxes. begun work on password prompt and data type validity check

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+//document is a built in object that allows you to access the webpage's content
+var generateBtn = document.querySelector("#generate"); //Assigns the html button element with the id "generate" to the variable generateBtn
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() { //the function responsible for password generation
+  var password = generatePassword(); //crates a variable password that is the result of the generatePassword function (line 14-16)
+  var passwordText = document.querySelector("#password"); //creates a passwordText variable then uses document.queryselector to select the css id #password and assigns it to the variable passwordText
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //when the button is clicked, the writePassword function is called
+
+function generatePassword(){
+ var lowercase = "abcdefghijklmnopqrstuvwxyz"; //variable containing a string of lowercase letters
+ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //variable containing a string of uppercase letters
+ var numbers = "0123456789"; //variable containing a string of numbers
+ var special = "!£$%^&*()_+=-{}[]:;@'~#<>,.|?/"; //variable containing a string of special characters
+
+ //the following code opens a box at the top of the webpage that asks the user if they would like to include a certain set of characters in their password
+ var lowercasePref = confirm("Click OK for lowercase characters to be included in your password or click cancel to move on to the next section")
+ var uppercasePref = confirm("Click OK for uppercase characters to be included in your password or click cancel to move on to the next section")
+ var numberPref = confirm("Click OK for numbers to be included in your password or click cancel to move on to the next section")
+ var specialPref = confirm("Finally, would you like to include special characters within your password?")
+
+ var passwordLength = prompt("How many characters would you like to include within your password?")
+
+ if (isNaN(passwordLength)) {
+  alert("please enter a valid number")
+ } else {
+ }
+}
+
+
+
+function checkPasswordLength() {
+  
+}
